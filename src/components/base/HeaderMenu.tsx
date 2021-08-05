4c9eeca7ed8b30f2ebd7db5440e0da5b51082abc
Danah <sweetmilky.dev@gmail.com>
2021-08-05 09:23:30 +0900

    header 로고영역 분리, hover css 수정

--- src/components/base/HeaderMenu.tsx
+++ src/components/base/HeaderMenu.tsx
@@ -8,6 +8,21 @@ import { Menu as MenuT } from 'types/common';
 const List = styled.div`
   ${tw`flex flex-row items-center`};
 `;
+const MenuBase = tw(Link)`h-12 px-2 whitespace-nowrap font-semibold`;
+const Logo = tw(MenuBase)`mr-2`;
+const Item = styled(MenuBase)<{ current: boolean }>`
+  ${tw`rounded-3xl`}
+  ${({ current }) => (current ? tw`bg-green-500 text-white` : tw`bg-white text-green-500`)};
+
+  :not(:last-of-type) {
+    margin-right: 4px;
+  }
+  :hover {
+    ${tw`transition duration-300 ease-in-out shadow-md cursor-pointer`};
+    ${({ current }) => !current && tw`bg-green-500 text-white`};
+  }
+`;
+const Inner = tw.div`h-full flex items-center`;
 
 interface HeaderMenuProps {
   homePath: string;
@@ -19,28 +34,17 @@ function HeaderMenu({ homePath, menu }: HeaderMenuProps) {
 
   return (
     <List>
-      {[{ path: homePath, name: 'Fixabay' }, ...menu].map(({ path, name }) => (
+      <Logo to={homePath}>
+        <Inner>Fixabay</Inner>
+      </Logo>
+      {menu.map(({ path, name }) => (
         <Menu key={name} path={path} name={name} current={path !== homePath && path === location.pathname} />
       ))}
     </List>
   );
 }
 
-const Item = styled(Link)<{ current: boolean }>`
-  ${tw`h-12 px-2 whitespace-nowrap rounded-3xl font-semibold`};
-  ${({ current }) => (current ? tw`bg-green-500 text-white shadow-md` : tw`text-green-500`)};
-
-  :not(:last-of-type) {
-    margin-right: 4px;
-  }
-  :hover {
-    ${tw`transition duration-300 ease-in-out shadow-md cursor-pointer`};
-    ${({ current }) => (current ? tw`bg-white text-green-500 shadow-none` : tw`bg-green-500 text-white`)};
-  }
-`;
-const Inner = tw.div`h-full flex items-center`;
-
-const Menu = memo(function Menu({ path, name, current }: { path: string; name: string; current: boolean }) {
+const Menu = memo(function Menu({ path, name, current = false }: { path: string; name: string; current?: boolean }) {
   return (
     <Item to={path} current={current}>
       <Inner>{name}</Inner>
